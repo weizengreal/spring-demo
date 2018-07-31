82338156d7bba21771f4a69664135f29085e03d2
weizeng <466730846@qq.com>
2018-07-31 20:24:32 +0800

    调整入口main函数的位置

--- src/main/java/com/woai662/Entry.java
+++ src/main/java/com/woai662/Entry.java
@@ -1,15 +1,13 @@
 package com.woai662.demo.configuration;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableDiscoveryClient
-@ComponentScan(basePackages = "com.woai662.demo")
+//@EnableAutoConfiguration
+//@EnableDiscoveryClient
+//@ComponentScan(basePackages = "com.woai662.demo")
 public class Entry {
 
     public static void main(String[] args) throws Exception {
