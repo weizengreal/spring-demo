01e904225ca6cbe0588b5019449df39c58ea9fed
weizeng <466730846@qq.com>
2018-07-31 17:34:09 +0800

    服务发现相关代码

--- src/main/java/com/woai662/demo/configuration/Entry.java
+++ src/main/java/com/woai662/demo/configuration/Entry.java
@@ -1,10 +1,14 @@
 package com.woai662.demo.configuration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.woai662.demo")
 public class Entry {
 
